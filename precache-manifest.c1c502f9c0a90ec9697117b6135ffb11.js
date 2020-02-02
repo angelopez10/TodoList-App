38783ad0be1a4ebf429ede6065ea020eed83f378
angelopez10 <angelopez10898@gmail.com>
2020-02-01 23:31:36 -0300

    Updates

---
+++ precache-manifest.c1c502f9c0a90ec9697117b6135ffb11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f86c55be90e5f33b26894d7fd36be67",
+    "url": "/TodoList-App/index.html"
+  },
+  {
+    "revision": "61cf06f7540121567285",
+    "url": "/TodoList-App/static/css/main.dc82aa96.chunk.css"
+  },
+  {
+    "revision": "bb37182fb9b7a4650bdf",
+    "url": "/TodoList-App/static/js/2.afaa6932.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TodoList-App/static/js/2.afaa6932.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61cf06f7540121567285",
+    "url": "/TodoList-App/static/js/main.5237e5c0.chunk.js"
+  },
+  {
+    "revision": "b91496d8638c8e6b1888",
+    "url": "/TodoList-App/static/js/runtime-main.9d69c39e.js"
+  }
+]);
