825a72e197be66e1d921ec1ee92d5964177d3d60
angelopez10 <angelopez10898@gmail.com>
2020-05-08 13:38:13 -0400

    'fixed'

--- Traffic-Light/traffic-light/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import TrafficLight from './TrafficLight.js'
-
-
-ReactDOM.render(<TrafficLight />, document.getElementById('root'));
-
-
